1674acf021cd05e8e191be134654a61281a4166d
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-23 16:25:34 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -12,10 +12,20 @@ class String
   end
 
   def question?
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
 
   end
 
